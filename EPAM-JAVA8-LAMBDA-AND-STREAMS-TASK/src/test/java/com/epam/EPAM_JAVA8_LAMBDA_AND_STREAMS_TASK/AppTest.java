254598cfc60e855a53f83cdaf3a28f7e1a9558b4
Sujith <170030811@kluniversity.in>
2020-07-25 23:33:40 +0530

    commit 1

---
+++ EPAM-JAVA8-LAMBDA-AND-STREAMS-TASK/src/test/java/com/epam/EPAM_JAVA8_LAMBDA_AND_STREAMS_TASK/AppTest.java
@@ -0,0 +1,20 @@
+package com.epam.EPAM_JAVA8_LAMBDA_AND_STREAMS_TASK;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
